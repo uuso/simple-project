90ba11cf2907789aa8e2a88cf182233010d9be06
uuso <mymail.uuso@gmail.com>
2019-12-23 14:37:17 +0300

    added Profile model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from myapp.models import Dummy
+from myapp.models import Dummy, UserProfile
 
 # Register your models here.
 admin.site.register(Dummy)
+admin.site.register(UserProfile)
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 class Dummy(models.Model):
     header = models.CharField(max_length=64, default="Header")
-    text = models.TextField(default='Veniam eiusmod irure est aliqua aliqua aliqua minim ut sit incididunt enim sit exercitation proident.')
+    text = models.TextField(default='Veniam eiusmod irure est aliqua aliqua aliqua \
+        minim ut sit incididunt enim sit exercitation proident.')
+
+class UserProfile(models.Model):
+    age = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
